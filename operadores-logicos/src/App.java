8d5cea68637fcafc4a22a88cde5ba640ab1ca92c
André Basseto <andre.basseto@hotmail.com>
2023-03-07 19:53:59 -0300

    operadores logicos

---
+++ operadores-logicos/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) {
+        /*
+         * conjuncao
+         * 
+         * disjunção
+         * 
+         * disjunção exclusiva
+         * 
+         * negação
+         */
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
+
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
+
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
+        System.out.println("b4 ^ b1 " + (b4 ^ b1));
+
+        System.out.println("!b1 " + (!b1));
+        System.out.println("!b2 " + (!b2));
+    }
+}
