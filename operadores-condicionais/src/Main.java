536ee3c8f5ee3416da9e306e378725adbdfd0abd
André Basseto <andre.basseto@hotmail.com>
2023-03-06 21:52:57 -0300

    operadores condicionais

---
+++ operadores-condicionais/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        float numero2 = 4.5f;
+        double numero3 = 59.40d;
+        char caractere1 = 'x';
+        String palavra = "teste";
+
+        System.out.println(numero1 > 10);
+        System.out.println(numero1 >= 10);
+        System.out.println(numero2 < 3);
+        System.out.println(numero3 == 59.40);
+        System.out.println(caractere1 == 'x');
+        System.out.println(palavra == "teste");
+    }
+}
